09fc76f6f58ef3cca32386f8266401d25575f27f
ykong1337 <ykonghao@icloud.com>
2023-10-06 10:17:24 +0800

    fmt

--- app/src/main/java/cc/ykong/lyred/MainActivity.kt
+++ app/src/main/java/cc/ykong/lyred/MainActivity.kt
@@ -37,6 +37,7 @@ class MainActivity : AppCompatActivity() {
         val up = findViewById<Button>(R.id.up)
         val down = findViewById<Button>(R.id.down)
         val reset = findViewById<Button>(R.id.reset)
+        val openFloat = findViewById<CheckBox>(R.id.open_float)
         this.format.roundingMode = RoundingMode.FLOOR
         findViewById<Button>(R.id.open).setOnClickListener {
             open(fileName, hit)
@@ -64,71 +65,9 @@ class MainActivity : AppCompatActivity() {
             setHit(hit)
         }
 
-        findViewById<CheckBox>(R.id.open_float).setOnCheckedChangeListener { _, b ->
+        openFloat.setOnCheckedChangeListener { _, b ->
             when (b) {
-                true -> {
-                    EasyFloat.with(this)
-                        .setTag("play")
-                        .setLayout(R.layout.float_play) {
-                            val play = it.findViewById<Button>(R.id.play)
-                            val save = it.findViewById<Button>(R.id.save)
-                            if (readLocation()) {
-                                save.text = "已读取"
-                            }
-                            play.setOnClickListener {
-                                if (!Control.pause && Control.is_play) {
-                                    Control.pause = true
-                                    play.text = "播放"
-                                } else {
-                                    Control.pause = false
-                                    play.text = "暂停"
-                                    if (!Control.playing) {
-                                        Control.is_play = true
-                                        midi.play()
-                                    }
-                                }
-                            }
-                            it.findViewById<Button>(R.id.stop).setOnClickListener {
-                                Control.is_play = false
-                                Control.pause = false
-                                play.text = "播放"
-                            }
-
-                            it.findViewById<Switch>(R.id.pos).setOnCheckedChangeListener { _, b ->
-                                when (b) {
-                                    true -> createLocation(this, "location")
-                                    false -> EasyFloat.dismiss("location", true)
-                                }
-                            }
-                            it.findViewById<CheckBox>(R.id.show).setOnCheckedChangeListener { a, b ->
-                                when (b) {
-                                    true -> {
-                                        Control.pos = true
-                                        Control.pos_count = 0
-                                        a.text = "正在设置+1"
-                                    }
-
-                                    false -> {
-                                        Control.pos = false
-                                        Control.pos_count = 0
-                                        a.text = "设置坐标"
-                                    }
-                                }
-                            }
-                            it.findViewById<Button>(R.id.save).setOnClickListener {
-                                Control.pos_count = 0
-                                saveLocation()
-                                save.text = "保存成功"
-                            }
-                            it.findViewById<Button>(R.id.open_f).setOnClickListener {
-                                play.text = "播放"
-                                open(fileName, hit)
-                            }
-                        }
-                        .setShowPattern(ShowPattern.ALL_TIME)
-                        .show()
-                }
-
+                true -> createFloat(fileName, hit)
                 false -> EasyFloat.dismiss("play", true)
             }
         }
@@ -137,11 +76,14 @@ class MainActivity : AppCompatActivity() {
         if (intent?.type == "audio/midi") {
             val path = data?.path
             if (path != null) {
+                EasyFloat.dismiss("play", true)
                 val name = path.substring(path.lastIndexOf('/') + 1)
                 contentResolver.openInputStream(data)?.let { this.midi.init(it) }
                 this.midi.offset = 0
                 fileName.text = "你选择的是: $name"
                 setHit(hit)
+                openFloat.isChecked = true
+                createFloat(fileName, hit)
                 Toast.makeText(this, "Midi: $name", Toast.LENGTH_LONG).show()
             }
         }
@@ -163,6 +105,69 @@ class MainActivity : AppCompatActivity() {
     private fun setHit(hit: TextView) {
         hit.text =
             "偏移量: " + this.midi.offset + " - " + "命中率: " + this.format.format(this.midi.detect(this.midi.offset) * 100) + "%"
+    }
+
+    private fun createFloat(fileName: TextView, hit: TextView) {
+        EasyFloat.with(this)
+            .setTag("play")
+            .setLayout(R.layout.float_play) {
+                val play = it.findViewById<Button>(R.id.play)
+                val save = it.findViewById<Button>(R.id.save)
+                if (readLocation()) {
+                    save.text = "已读取"
+                }
+                play.setOnClickListener {
+                    if (!Control.pause && Control.is_play) {
+                        Control.pause = true
+                        play.text = "播放"
+                    } else {
+                        Control.pause = false
+                        play.text = "暂停"
+                        if (!Control.playing) {
+                            Control.is_play = true
+                            midi.play()
+                        }
+                    }
+                }
+                it.findViewById<Button>(R.id.stop).setOnClickListener {
+                    Control.is_play = false
+                    Control.pause = false
+                    play.text = "播放"
+                }
+
+                it.findViewById<Switch>(R.id.pos).setOnCheckedChangeListener { _, b ->
+                    when (b) {
+                        true -> createLocation(this, "location")
+                        false -> EasyFloat.dismiss("location", true)
+                    }
+                }
+                it.findViewById<CheckBox>(R.id.show).setOnCheckedChangeListener { a, b ->
+                    when (b) {
+                        true -> {
+                            Control.pos = true
+                            Control.pos_count = 0
+                            a.text = "正在设置+1"
+                        }
+
+                        false -> {
+                            Control.pos = false
+                            Control.pos_count = 0
+                            a.text = "设置坐标"
+                        }
+                    }
+                }
+                it.findViewById<Button>(R.id.save).setOnClickListener {
+                    Control.pos_count = 0
+                    saveLocation()
+                    save.text = "保存成功"
+                }
+                it.findViewById<Button>(R.id.open_f).setOnClickListener {
+                    play.text = "播放"
+                    open(fileName, hit)
+                }
+            }
+            .setShowPattern(ShowPattern.ALL_TIME)
+            .show()
     }
 }
 
